72f673a937bc3b2fb9e8af537fc06a8b80bc4b09
akrck02 <aketza.vazquez@gmail.com>
2024-03-30 11:27:44 +0100

    [ci] testing publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
  
- version=$(cat version.txt)
+ version=$(cat version.config)
  echo "Publishing version ${version}"
+ echo go list -m github.com/akrck02/valhalla-core-sdk@${version} 
+
  GOPROXY=proxy.golang.org go list -m github.com/akrck02/valhalla-core-sdk@${version}
