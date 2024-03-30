598d92ff0726e6c5a616160bdb58a65ad3a62df7
akrck02 <43274508+akrck02@users.noreply.github.com>
2024-03-30 13:08:03 +0100

    Feature/ci publish (#1)
    * [ci] publish test
    
    * [ci] publish test
    
    * Fix branch names in publish_version.yaml
    
    * [error] adding errors to SDK
    
    * [version] v0.0.4
    
    * [fix] no publish to golang.org

---
+++ error/general.go
@@ -0,0 +1,10 @@
+package error
+
+type General int
+
+const (
+	UNEXPECTED_ERROR = 000
+	ACCESS_DENIED    = 001
+	NOT_IMPLEMENTED  = 002
+	INVALID_REQUEST  = 003
+)
--- publish.sh
+++
@@ -1,6 +0,0 @@
- 
- version=$(cat version.config)
- echo "Publishing version ${version}"
- echo go list -m github.com/akrck02/valhalla-core-sdk@${version} 
-
- GOPROXY=proxy.golang.org go list -m github.com/akrck02/valhalla-core-sdk@${version}
