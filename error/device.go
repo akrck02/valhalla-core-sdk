47a6777b597e33dc9b17763f1e2ccf86f995ee2f
akrck02 <aketza.vazquez@gmail.com>
2024-05-26 14:01:51 +0200

    adding DATABASE_ERROR and DEVICE_DOES_NOT_EXIST codes

---
+++ error/device.go
@@ -0,0 +1,7 @@
+package error
+
+type Device int
+
+const (
+	DEVICE_DOES_NOT_EXIST = 900
+)
--- error/team.go
+++ error/team.go
@@ -3,23 +3,23 @@ package error
 type Team int
 
 const (
-	NO_PERMISSION          = 630
-	TEAM_ALREADY_EXISTS    = 631
-	EMPTY_TEAM_NAME        = 632
-	NO_OWNER               = 633
-	OWNER_DOESNT_EXIST     = 634
-	BAD_OBJECT_ID          = 635
-	UPDATE_ERROR           = 636
-	TEAM_NOT_FOUND         = 637
-	SHORT_NAME             = 638
-	LONG_NAME              = 639
-	SHORT_DESCRIPTION      = 640
-	LONG_DESCRIPTION       = 641
-	EMPTY_TEAM_DESCRIPTION = 642
-	NO_MEMBER              = 643
-	NO_TEAM                = 644
-	NO_PROJECT             = 645
-	USER_IS_OWNER          = 646
-	USER_ALREADY_MEMBER    = 647
-	TEAM_SEARCH_ERROR      = 648
+	NO_PERMISSION          = 800
+	TEAM_ALREADY_EXISTS    = 801
+	EMPTY_TEAM_NAME        = 802
+	NO_OWNER               = 803
+	OWNER_DOESNT_EXIST     = 804
+	BAD_OBJECT_ID          = 805
+	UPDATE_ERROR           = 806
+	TEAM_NOT_FOUND         = 807
+	SHORT_NAME             = 808
+	LONG_NAME              = 809
+	SHORT_DESCRIPTION      = 810
+	LONG_DESCRIPTION       = 811
+	EMPTY_TEAM_DESCRIPTION = 812
+	NO_MEMBER              = 813
+	NO_TEAM                = 814
+	NO_PROJECT             = 815
+	USER_IS_OWNER          = 816
+	USER_ALREADY_MEMBER    = 817
+	TEAM_SEARCH_ERROR      = 818
 )
