4cc9a05a559d08aae6a29c4fbb3ba0f78cf2f3b7
akrck02 <aketza.vazquez@gmail.com>
2024-06-03 09:07:06 +0200

    order and cleaning on valhalla errors

--- valerror/device.go
+++ valerror/device.go
@@ -2,7 +2,8 @@ package valerror
 
 type Device int
 
+// 5000 --> 5999
 const (
-	DEVICE_DOES_NOT_EXIST      = 900
-	CANNOT_GENERATE_AUTH_TOKEN = 901
+	DEVICE_DOES_NOT_EXIST      = 5000
+	CANNOT_GENERATE_AUTH_TOKEN = 5001
 )
--- valerror/role.go
+++ valerror/role.go
@@ -2,4 +2,5 @@ package valerror
 
 type Role int
 
+// 5000 --> 5999
 const ()
