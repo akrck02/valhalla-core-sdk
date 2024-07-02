263c433107af1e8f4f04f6a803c3ac4a327eedd9
akrck02 <aketza.vazquez@gmail.com>
2024-07-02 21:17:40 +0200

    added client to database context

--- models/system/database_context.go
+++ models/system/database_context.go
@@ -1,8 +1,10 @@
 package systemmodels
 
-import "context"
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type Database struct {
-	Name    string `json:"name"`
-	Context context.Context
+	Name   string `json:"name"`
+	Client *mongo.Client
 }
