8812d64321404614af1fa5256db249f14c0bf1a4
Itros97 <itros97@gmail.com>
2024-07-09 22:27:30 +0200

    Added ID to project model

--- models/project.go
+++ models/project.go
@@ -1,6 +1,7 @@
 package models
 
 type Project struct {
+	ID          string   `bson:"_id,omitempty"`
 	Name        string   `bson:"name,omitempty"`
 	Description string   `bson:"description,omitempty"`
 	Owner       string   `bson:"owner,omitempty"`
