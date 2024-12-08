74319a1d791e52e48f3ed1881e8ad27e04e9a9b3
akrck02 <aketza.vazquez@gmail.com>
2024-12-08 20:03:11 +0100

    repository: device updated

--- src/main/kotlin/org/akrck02/valhalla/core/sdk/repository/DeviceRepository.kt
+++ src/main/kotlin/org/akrck02/valhalla/core/sdk/repository/DeviceRepository.kt
@@ -10,38 +10,27 @@ import org.akrck02.valhalla.core.sdk.model.device.Device
 interface DeviceRepository {
 
     /**
-     * Register a new device
+     * Register a new device for a user
+     * @param userId The user id to add the                                                                                                                                        device to
      * @param device The device to register
      * @return The id of the registered device
      */
-    suspend fun register(device: Device?): String
+    suspend fun register(userId: String?, device: Device?): String
 
     /**
-     * Get a device by id
+     * Get a device by user and id
+     * @param userId The user id
      * @param id The device id
      * @return The requested device
      */
-    suspend fun get(id: String?): Device
+    suspend fun get(userId: String?, id: String?): Device
 
     /**
-     * Get a device by its current auth token
+     * Get a device by its user and current auth token
+     * @param userId The user id
      * @param token The auth token
-     * @return the requested device
+     * @return The requested device
      */
-    suspend fun getByAuth(token: String?): Device
-
-    /**
-     * Get all the devices belonging to a user
-     * @param userId The owner user id
-     * @return The list of devices
-     */
-    suspend fun getAllByOwner(userId: String?): List<Device>
-
-    /**
-     * Update a device
-     * @param id The device id
-     * @param device The new device data
-     */
-    suspend fun update(id: String?, device: Device?)
+    suspend fun getByAuth(userId: String?, token: String?): Device
 
 }
