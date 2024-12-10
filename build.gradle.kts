5a788adf0c59eb0c561d026aa2b331fc33080480
akrck02 <aketza.vazquez@gmail.com>
2024-12-10 20:56:31 +0100

    gradle: scripts set

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+import java.io.FileInputStream
+import java.util.*
+
+// Variables section
+val localProperties: Properties = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "local.properties")))
+}
+
+group = providers.gradleProperty("organization").getOrElse("")
+version = providers.gradleProperty("valhalla.core.sdk.version").getOrElse("")
+
+val jdkVersion: Int = providers.gradleProperty("jdk.version").getOrElse("").toInt()
+val mavenServerName: String = providers.gradleProperty("maven.server.name").getOrElse("")
+val mavenServerUrl: String = providers.gradleProperty("maven.server.url").getOrElse("")
+val mavenServerUser: String = localProperties.getProperty("maven.server.user")
+val mavenServerPassword: String = localProperties.getProperty("maven.server.password")
+
+// Plugins section
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.0.21"
+    id("maven-publish")
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
+}
+
+// Repositories
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+// Testing section
+tasks.test {
+    useJUnit()
+}
+
+// Compilation section
+kotlin {
+    jvmToolchain(jdkVersion)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+// Publishing section
+publishing {
+    repositories {
+        mavenLocal()
+        maven {
+            name = mavenServerName
+            url = uri(mavenServerUrl)
+            credentials {
+                username = mavenServerUser
+                password = mavenServerPassword
+            }
+            isAllowInsecureProtocol = true
+        }
+    }
+    publications.withType<MavenPublication> {
+        from(components["java"])
+    }
+}
+
+// Dependency section
+dependencies {
+
+    // Tests
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Kotlin coroutines
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.2.1")
+
+    // Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.mongodb:bson:5.2.1")
+}
+
+
+
+
